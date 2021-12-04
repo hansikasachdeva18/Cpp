52438c1b8f958e511229382454df08f20738ffe4
Hansika Sachdeva <86574049+hansikasachdeva18@users.noreply.github.com>
2021-12-05 00:49:27 +0530

    Add files via upload

---
+++ Patterns_1/SquarePattern2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << n - j + 1;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
+
+// 4321
+// 4321
+// 4321
+// 4321
